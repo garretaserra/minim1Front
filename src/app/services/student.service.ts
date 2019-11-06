c35dfae8f4bb51df44552ab3eb37f9c7e5685ca1
sergi <sergi@unievegroup.com>
2019-11-06 23:52:07 +0100

    Added function to get all students in the student service

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Url} from "./url";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Student} from "../models/Student";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  url: Url;
+
+  constructor(private http: HttpClient) {
+    this.url = new Url();
+  }
+
+  getSubjects(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.url.url+'/student/get');
+  }
 }
